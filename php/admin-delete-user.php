974a95dfdf35139cbcb11f9984241dcf8993625f
RAJDEEP KAUR <33704740+kaurrajdeep@users.noreply.github.com>
2018-05-24 18:31:18 -0700

    Delete user_preferences, user time constraints when user deleted
    https://github.com/USC-CSSL/Automatic-TA-assignment/issues/35
    When user is deleted, delete entry from TA, TA_Time_Constraints, TA_Preferences as well.

--- php/admin-delete-user.php
+++ php/admin-delete-user.php
@@ -5,7 +5,14 @@
 	{
 		$userId=$_POST["user_id"];
 		$sql1="DELETE FROM `User` WHERE `User_Id`='$userId'";
+        $ta_id = "SELECT TA_Id from TA where `User_Id`='$userId'";
 		$sql2="DELETE FROM `TA` WHERE `User_Id`='$userId'";
+        if(!empty($ta_id)) {
+            $sql3="DELETE FROM `TA_Time_Constraints` WHERE `TA_Id`='$ta_id'";
+            $sql4="DELETE FROM `TA_Preferences` WHERE `TA_Id`='$ta_id'";
+            $result3=mysqli_query($conn,$sql3);
+		    $result4=mysqli_query($conn,$sql4);
+        }
 			
 		$result1=mysqli_query($conn,$sql1);
 		$result2=mysqli_query($conn,$sql2);
