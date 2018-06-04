05f58e206db443d836bfd2cd6f2b36f822258f2e
RAJDEEP KAUR <33704740+kaurrajdeep@users.noreply.github.com>
2018-06-04 12:44:45 -0700

    Delete TA pref\Time Constraints when deactivating
    https://github.com/USC-CSSL/Automatic-TA-assignment/issues/35#

--- php/admin-deactivate-user.php
+++ php/admin-deactivate-user.php
@@ -8,8 +8,13 @@
 		$active=0;
 		$sql="UPDATE `TA` SET `IsActive`='$active' WHERE `TA_Id`='$ta'";
 		$result=mysqli_query($conn,$sql);
+                $sql3="DELETE FROM `TA_Time_Constraints` WHERE `TA_Id`='$ta'";
+                $sql4="DELETE FROM `TA_Preferences` WHERE `TA_Id`='$ta'";
+                $result3=mysqli_query($conn,$sql3);
+                $result4=mysqli_query($conn,$sql4);
+        
 		echo $result;
-		if($result)
+		if($result && $result3 && $result4)
 		{
 			#echo "<script> alert('Activation done');</script>";
 			echo '<META HTTP-EQUIV="Refresh" Content="0; URL=admin-view-users.php">';
@@ -17,7 +22,7 @@
 		else
 		{
 			
-			echo "<script> alert('Error Activation');</script>";
+			echo "<script> alert('Error DeActivation');</script>";
 			#header('Location: ' . $_SERVER["HTTP_REFERER"] );
 		
 		}
