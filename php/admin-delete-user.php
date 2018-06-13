cc011a86b5b8a4e6cc1f4ea45759374030d31e77
RAJDEEP KAUR <33704740+kaurrajdeep@users.noreply.github.com>
2018-06-12 17:25:43 -0700

    Correct call to db to fetch TA_Id
    https://github.com/USC-CSSL/Automatic-TA-assignment/issues/33

--- php/admin-delete-user.php
+++ php/admin-delete-user.php
@@ -5,13 +5,21 @@
 	{
 		$userId=$_POST["user_id"];
 		$sql1="DELETE FROM `User` WHERE `User_Id`='$userId'";
-        $ta_id = "SELECT TA_Id from TA where `User_Id`='$userId'";
+               
+        $ta_sql = "SELECT TA_Id from TA where `User_Id`='$userId'";
+        $result=mysqli_query($conn,$ta_sql);
+        $row = mysqli_fetch_array($result);
+        
+        $ta_id = $row['TA_Id'];
+        
 		$sql2="DELETE FROM `TA` WHERE `User_Id`='$userId'";
+        echo $ta_id;
         if(!empty($ta_id)) {
             $sql3="DELETE FROM `TA_Time_Constraints` WHERE `TA_Id`='$ta_id'";
             $sql4="DELETE FROM `TA_Preferences` WHERE `TA_Id`='$ta_id'";
             $result3=mysqli_query($conn,$sql3);
 		    $result4=mysqli_query($conn,$sql4);
+            echo 'inside';
         }
 			
 		$result1=mysqli_query($conn,$sql1);
@@ -21,13 +29,13 @@
 		{
 			if($result2)
 			{
-				echo "<script> alert('Record Deleted.');</script>";
-				echo '<META HTTP-EQUIV="Refresh" Content="0; URL=admin-view-users.php">';
+				//echo "<script> alert('Record Deleted.');</script>";
+				//echo '<META HTTP-EQUIV="Refresh" Content="0; URL=admin-view-users.php">';
 			}
 			else
 			{	
-				echo "<script> alert('Error Deleting record from TA Database.');</script>";
-				echo '<META HTTP-EQUIV="Refresh" Content="0; URL=admin-view-users.php">';
+				//echo "<script> alert('Error Deleting record from TA Database.');</script>";
+				//echo '<META HTTP-EQUIV="Refresh" Content="0; URL=admin-view-users.php">';
 			}
 		}
 		else
